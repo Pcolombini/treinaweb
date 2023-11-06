0938bd5c272bf6a5d3a826f5e85d4e141774ab2a
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-06 20:37:12 -0300

    Primeiro commit no repo do curso treinaweb
    Subindo arquivos de intro front e php

---
+++ PHP-fundamentos/iniciando-php/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="stylesheet" href="style.css">
+	<title>Iniciando com PHP</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Cutive&display=swap" rel="stylesheet">
+</style>
+</head>
+
+<body>
+
+	<header>
+		<h1 class="title">
+			<?= 'Olá Mundo <span class="span" style="color:red ;">PHP!</span>' ?>
+		</h1>
+	</header>
+
+	<main>
+		
+	</main>
+
+	<footer>
+		
+	</footer>
+
+</body>
+
+</html>
