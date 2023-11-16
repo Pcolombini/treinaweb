60205d19221846e1b69ab4b9a6b1b1ef155975f0
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-16 13:45:39 -0300

    objetos e referencia

--- formacaoPHP/php_oo/referencia.php
+++ formacaoPHP/php_oo/referencia.php
@@ -1,4 +1,6 @@
-<?php 
+<section>
+    <p>
+    <?php 
 
 // ADICIONANDO VALOR À VARIÁVEL B POR REFERÊNCA DE A;
 
@@ -15,4 +17,27 @@ echo "Valor de A = $a e Valor de B = $b após alterar somente B<br>";
 
 // ISSO ACONTECE POR QUE O VALOR É O MESMO, E AS DUAS VARIÁVEIS APONTAM PARA O MESMO LOCAL NA MEMÓRIA, E A ALTERAÇÃO É FEITA NO ENDEREÇO DA MEMÓRIA
 
+echo '------------------------------------------------------------';
 ?>
+    </p>
+</section>
+
+<section>
+    <p>
+        <?php
+    
+            echo "<html lang='pt-br'>";
+    
+            require_once './src/Classes/Produto.php';
+            require_once './src/Classes/Cliente.php';
+            require_once './src/Classes/Fornecedor.php';
+    
+            $prod1 = new Produto();
+    
+            $prod1->titulo = "Skol";
+            $prod1->descricao = "Cerveja Pilsen";
+            $prod1->preco = 2.5;
+    
+        ?>
+    </p>
+</section>
