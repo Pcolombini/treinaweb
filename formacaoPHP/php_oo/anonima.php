2993096990820ba52ac617d0c9ecc05ab9e0615b
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-16 21:13:59 -0300

    fix

--- formacaoPHP/php_oo/anonima.php
+++ formacaoPHP/php_oo/anonima.php
@@ -1,10 +1,51 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-require_once('src/Classes/Fornecedor.php');
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-$fornecedor = new Fornecedor;
+<body>
+    <main>
+        <section class="formulario">
+            <form action="" method="get">
+                <section>
+                    <label for="">NOME
+                        <input type="text" name="nome" id="">
+                    </label>
+                </section>
+                <section>
+                    <label for="">VALOR
+                        <input type="text" name="valor" id="">
+                    </label>
+                </section>
+                <section>
+                    <label for="">DESCONTO
+                        <input type="text" name="desconto" id="">
+                    </label>
+                </section>
+                <section>
+                    <button type="submit">enviar</button>
+                </section>
+            </form>
+        </section>
+        <section>
+            <?php
 
-$fornecedor->autorizar(new class {
-    public $nome = "João";
-    public $senha = "123456";
-});
+            // require_once('src/Classes/Fornecedor.php');
+
+            // $fornecedor = new Fornecedor;
+
+            // $fornecedor->autorizar(new class {
+            //     public $nome = "João";
+            //     public $senha = "123456";
+            // });
+
+            ?>
+        </section>
+    </main>
+</body>
+
+</html>
