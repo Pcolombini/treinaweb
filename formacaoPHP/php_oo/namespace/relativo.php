c7ec41066ecb23ea8aeebf3ba936c32b3b11fb6d
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-22 18:19:16 -0300

    Namespace completo e relativo

---
+++ formacaoPHP/php_oo/namespace/relativo.php
@@ -0,0 +1,11 @@
+<?php 
+
+require '../src/Email/Envio.php';
+require '../src/Email/Transporte.php';
+require '..\src\Email\Adaptadores\Mailgun\Adaptador.php';
+
+$envio = new \App\Email\Envio;
+
+$envio->enviar();
+
+var_dump($envio);
--- formacaoPHP/php_oo/src/Email/Envio.php
+++ formacaoPHP/php_oo/src/Email/Envio.php
@@ -5,7 +5,14 @@ namespace App\Email;
 const VERSAO = 1.0;
 class Envio
 {
+    public function enviar(): void
+    {
+        $adptador = new Adaptadores\Mailgun\Adptador;
 
+        $transporte = new Transporte;
+
+        var_dump($adptador, $transporte);
+    }
 }
 
 function validar(string $mail): void
