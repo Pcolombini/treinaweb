bff5231f1d24fc2104e1aba6395fb8d0d9df8893
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-28 20:53:18 -0300

    Trabalhando com herança e usando o extends

--- formacaoPHP/php_oo_2/src/Classes/Produto.php
+++ formacaoPHP/php_oo_2/src/Classes/Produto.php
@@ -8,10 +8,11 @@ class Produto
     public string $titulo;
     public string $descricao = "Cerveja brasileira";
     public float $preco;
+    public string $codigoBarras;
 
     public function defineCodigoDeBarras(string $codigo): void
     {
-        //$this->codigoBarras = $codigo;
+        $this->codigoBarras = $codigo;
     }
 
     public function acessaCodigoBarras(): void
