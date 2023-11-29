98f4d6be12e805d1abe4468866580461c1f79d4d
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-11-28 21:21:48 -0300

    Definindo estututa de herança

--- formacaoPHP/php_oo_2/heranca/extensao.php
+++ formacaoPHP/php_oo_2/heranca/extensao.php
@@ -1,16 +1,18 @@
 <?php
 
 use App\Classes\Eletrodomestico;
+use App\Classes\Microondas;
 
 require_once "../autoload/autoload-psr4.php";
 
 // $obj = new App\Classes\Eletrodomestico;
 
-$obj = new Eletrodomestico;
+$obj = new Microondas;
 
 $obj->titulo = "Geladeira";
 $obj->preco = 900.00;
 $obj->defineCodigoDeBarras("00112");
 $obj->definirVoltagem(220);
+$obj->definirPotencia(620);
 
 var_dump($obj);
