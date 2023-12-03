78f18b0d895350c93ae3b85337afa8633c195899
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-12-03 17:43:11 -0300

    Laços de repetição

---
+++ Desenvolvedor JavaScript/JavaScript Fundamentos/lacosRepeticao/main.js
@@ -0,0 +1,74 @@
+// function addHeader() {
+
+//     titulo = document.createElement('h1');
+
+//     addElemente = document.getElementById('header').appendChild(titulo);
+
+//     titulo.appendChild(document.createTextNode('Laços de Repetição'));
+
+// }
+
+// addHeader();
+
+//  INICIALIZAÇÃO ; EXPRESSÃO ; INCREMENTO
+for (let i = 0    ;  i <= 20   ;  i++) {
+    var a = 1000;
+}
+
+for (let i = 1; i <= 3; i++) {
+
+    for (let j = 1; j <= 10; j++) {
+
+        // console.log(`${i} x ${j} = ${i*j}`); 
+
+    }
+
+}
+
+let i = 0;
+
+/* while (i <= 10) {
+    console.log(i);
+    i++;
+} */
+
+let palavra = 'paralelepipedo';
+let letra = 'e';
+let total = 0;
+let totalAProcurar = 2;
+let indice = 0;
+
+while (total < totalAProcurar && indice < palavra.length) {
+
+    if (palavra[indice] === letra) {
+        total++;
+    }
+
+    indice++;
+}
+
+if (total >= totalAProcurar) {
+    // console.log("SIM");
+} else {
+    // console.log("NÃO");
+}
+
+let numeroEsclhido = Math.floor(Math.random() * 9 + 1),
+    acertou = false,
+    palpite = '3' ;
+
+do {
+
+    palpite;
+
+    if (parseInt(palpite) === numeroEsclhido) {
+        acertou = true;
+    }
+
+} while (palpite !== '' && parseInt(palpite) !== numeroEsclhido);
+
+if (acertou) {
+    console.log('Vitória');
+} else {
+    console.log('Não foi dessa vez');
+}
