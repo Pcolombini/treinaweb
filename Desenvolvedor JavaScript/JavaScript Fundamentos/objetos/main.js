18386b44a52b8aa04da2ae479c52483571e4678e
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-12-09 19:04:31 -0300

    Criando objetos com atributos e funções

--- Desenvolvedor JavaScript/JavaScript Fundamentos/objetos/main.js
+++ Desenvolvedor JavaScript/JavaScript Fundamentos/objetos/main.js
@@ -1,5 +1,48 @@
-let lapis = {
+// Criando o objeto
+
+const lapis = {
+    // propriedade chave: valor
     cor: 'Preto',
+    tamanho: 15,
+
+    /* funções escrita de forma simples, funções podem ser escritas como nome: function () {} ou arrow nome: () => {} */
+
+    escrever() {
+        console.log('Escrevendo...');
+    },
+
+    apagar:function(){
+        console.log('Apagando...');
+    },
+
+    rabiscar: () => {
+        console.log('Rabiscando...');
+    }
 };
 
-console.log(lapis);
+// Acessando os atributos e funções
+
+lapis.cor;
+lapis['cor'];
+
+let cor = 'cor';
+
+lapis[cor];
+
+// Criando novos atributos
+
+lapis.corDaBorracha = 'Rosa';
+lapis['Ponta'] = 1.5;
+
+// Removendo atributos
+
+delete lapis.Ponta;
+
+// Para saber o nome de todas as chaves que um objeto possue
+
+console.log(Object.keys(lapis));
+
+// 
+
+console.log(lapis)
+console.log(lapis['corDaBorracha']);
